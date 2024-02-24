cb029f9d0fe37b5713cc02be159e628b8191e199
Tomas Hudak <tomas.hudak@codera.ma>
2024-02-24 15:48:25 +0100

    initial commit

---
+++ NMHAssignment.WebAPI/Controllers/CalculationController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NMHAssignment.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculationController : ControllerBase
+    {
+
+    }
+}
